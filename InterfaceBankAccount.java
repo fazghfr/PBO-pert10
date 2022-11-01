c40d002401dbe64bd99be7092007e94cf225f256
Ahmad Fauzan Alghifari <96367502+fazghfr@users.noreply.github.com>
2022-11-01 17:15:10 +0700

    Add files via upload

---
+++ InterfaceBankAccount.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package account;
+
+/**
+ *
+ * @author fauza
+ */
+interface InterfaceBankAccount {
+    public final String BANK = "JavaBank Indonesia";
+    
+    public void deposit(int amt);
+    public void withdraw(int amt);
+    public int getBalance();
+    public String getBankName();
+}
